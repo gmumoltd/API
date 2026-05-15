6575c48902d791a65b78576c607bd6522521c4c1
unknown <gmumoltd@gmail.com>
2026-05-15 14:45:23 +0300

    trying to get all user data to show

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,26 @@
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then(response => response.json())
+// .then(data => {
+//     // fetches data to thr console
+//      console.log(data)
+//     })
+
+//     // fetching data for user
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then(response => response.json())
+// .then(data => {    
+//     data.forEach(user => {
+// document.getElementById("users").innerHTML += `<p>${user.name}</p>`
+//     })
+// })
+
 fetch("https://jsonplaceholder.typicode.com/users")
 .then(response => response.json())
-.then(data => {
-    console.log(data)
+.then(data => {    
+    // JSON.stringify(data, filter, indentation_spaces)
+    // Using 'null' and '4' formats the raw text with clean visual indents
+     data.forEach(user => {
+    document.getElementById("users").innerHTML += `<p>${data, null, 4}</p>`
 })
+})
+// .catch(error => console.error("Error fetching data:", error))
